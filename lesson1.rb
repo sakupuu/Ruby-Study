d4d8d77eb0bdad56fe6bf4675e834e78cf8644ef
sakura inoue <saku.yuzu.113@gmail.com>
2021-03-01 20:48:15 +0900

    条件分岐

---
+++ lesson1.rb
@@ -0,0 +1,20 @@
+puts "hoge"
+
+# 標準入力
+# gets
+
+# 改行コ-ドを消す
+# word = gets.chomp!
+# p word
+
+# 条件分岐
+# if gets == 3
+#   puts "さん"
+# end
+
+# puts gets
+# p gets.chomp!.to_i
+
+if gets.chomp!.to_i == 3
+  p  "さん"
+end
